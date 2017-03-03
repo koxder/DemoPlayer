01bf4af21040e5ce3b098f3cbb81e657c1796c99
Castor <cduran@arantia.com>
2017-03-03 14:07:27 +0100

    cambios

--- content/scripts/configuration/Configuration.js
+++ content/scripts/configuration/Configuration.js
@@ -1,10 +1,10 @@
 var ConfigurationManager=
 	(function(){
 		configuration = {
-						 "API_KEY":"AIzaSyDNXKuTy_oOkeo8WWrWJehz2oJUz3FVTDg",
+						 "API_KEY":"",
 						 "URL_GOOGLE":"https://www.googleapis.com/language/translate/v2?",
 						 "URL_BACKEND":"https://demo5131397.mockable.io/videos",
-						 "OMDB_API":"http://www.omdbapi.com/"
+						 "OMDB_API":"https://www.omdbapi.com/"
 						};
 
 	
